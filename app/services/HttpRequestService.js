cdc007fe5c9ebc6e28ffdc69ed1b4ebf96afe202
David Vuong <david.vuong256@gmail.com>
2017-05-28 22:59:25 +1000

    feat(HttpRequestService): add initial service impl

---
+++ app/services/HttpRequestService.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+import superagent from 'superagent';
+import Promise from 'bluebird';
+
+class HttpRequestService {
+  _request(endpoint, method, payload, headers) {
+    const request = {
+      get: superagent.get,
+      post: superagent.post,
+      put: superagent.put,
+      delete: superagent.delete,
+    }[method](endpoint);
+
+    request.set('Content-Type', 'application/json');
+    _.map(headers, (value, key) => {
+      request.set(key, value);
+    });
+
+    if (_.includes(['post', 'put'], method) && !_.isEmpty(payload)) {
+      request.send(payload);
+    }
+
+    return new Promise((resolve, reject) => {
+      request.end((err, res) => {
+        err ? reject(err) : resolve(res);
+      });
+    });
+  }
+
+  get(endpoint, headers = {}) {
+    return this._request(endpoint, 'get', {}, headers);
+  }
+
+  post(endpoint, payload = {}, headers = {}) {
+    return this._request(endpoint, 'post', payload, headers);
+  }
+
+  put(endpoint, payload = {}, headers) {
+    return this._request(endpoint, 'put', payload, headers);
+  }
+}
+
+export default new HttpRequestService()
