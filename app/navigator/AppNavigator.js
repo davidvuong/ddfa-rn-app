519c6eda71fb75711f156fdc086f81af2bede2cc
David Vuong <david.vuong256@gmail.com>
2017-06-03 14:41:47 +1000

    feat(navigator): lazy evalaute tabs

--- app/navigator/AppNavigator.js
+++ app/navigator/AppNavigator.js
@@ -7,9 +7,12 @@ import LoginScreen from '../screens/LoginScreen/LoginScreen';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import CheckInScreen from '../screens/CheckInScreen/CheckInScreen';
 
+// https://github.com/react-community/react-navigation/blob/master/docs/api/navigators/TabNavigator.md#tabnavigatorconfig
 const BottomTabNavigator = TabNavigator({
   Home: { screen: HomeScreen },
   CheckInScreen: { screen: CheckInScreen },
+}, {
+  lazy: true,
 });
 
 export default StackNavigator({
