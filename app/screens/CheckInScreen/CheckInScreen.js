f55ee970f14e3ad7a935349e6d8fc175d43bd6f6
David Vuong <david.vuong256@gmail.com>
2017-06-03 14:42:48 +1000

    feat(CheckInScreen): CheckIn prompts for location access

--- app/screens/CheckInScreen/CheckInScreen.js
+++ app/screens/CheckInScreen/CheckInScreen.js
@@ -6,6 +6,15 @@ import {
   Icon
 } from '@shoutem/ui'
 
+import GeoLocationService from '../../services/GeoLocationService';
+
+const CURRENT_POSITION_STATUS = {
+  PENDING: 'PENDING',
+  IN_PROGRESS: 'IN_PROGRESS',
+  ERROR: 'ERROR',
+  SUCCESS: 'SUCCESS'
+};
+
 export default class CheckInScreen extends Component {
   static navigationOptions = {
     title: 'Check-in',
@@ -18,13 +27,39 @@ export default class CheckInScreen extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      currentPosition: null,
+      currentPositionStatus: CURRENT_POSITION_STATUS.PENDING,
+    };
+  }
+
+  componentDidMount() {
+    this._getCurrentLocation();
+  }
+
+  _getCurrentLocation() {
+    this.setState({ currentPositionStatus: CURRENT_POSITION_STATUS.IN_PROGRESS });
+
+    GeoLocationService.promptLocationAccess().then(() => {
+      return GeoLocationService.getCurrentLocation();
+    }).then((position) => {
+      this.setState({
+        currentPosition: position,
+        currentPositionStatus: CURRENT_POSITION_STATUS.SUCCESS,
+      });
+    }, () => {
+      this.setState({
+        currentPositionStatus: CURRENT_POSITION_STATUS.ERROR,
+      });
+    });
   }
 
   render() {
     return (
       <View styleName="fill-parent vertical horizontal v-center">
         <Text>Welcome to the check-in screen!</Text>
+        <Text>{this.state.currentPositionStatus}</Text>
+        <Text>{this.state.currentPosition}</Text>
       </View>
     );
   }
