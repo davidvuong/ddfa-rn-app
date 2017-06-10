bd3369d521fe7c28b2206dda441b88342a0c3d01
David Vuong <david.vuong256@gmail.com>
2017-06-10 10:59:30 +1000

    rm(screens): remove screens that aren't used

--- app/navigator/AppNavigator.js
+++ app/navigator/AppNavigator.js
@@ -5,15 +5,11 @@ import {
 import LoginScreen from '../screens/LoginScreen/LoginScreen';
 import FeedScreen from '../screens/FeedScreen/FeedScreen';
 import CheckInScreen from '../screens/CheckInScreen/CheckInScreen';
-import ReviewsScreen from '../screens/ReviewsScreen/ReviewsScreen';
-import ProfileScreen from '../screens/ProfileScreen/ProfileScreen';
 
 // https://github.com/react-community/react-navigation/blob/master/docs/api/navigators/TabNavigator.md#tabnavigatorconfig
 const BottomTabNavigator = TabNavigator({
   FeedScreen: { screen: FeedScreen },
   CheckInScreen: { screen: CheckInScreen },
-  ReviewsScreen: { screen: ReviewsScreen },
-  ProfileScreen: { screen: ProfileScreen },
 }, {
   lazy: true,
 });
