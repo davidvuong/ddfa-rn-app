2e84c40c27d28d1ac3c7fa95718bfa67a7ea0480
David Vuong <david.vuong256@gmail.com>
2017-06-15 00:27:16 +1000

    feat(FeedScreen): removed header, pulled button to screen

--- app/screens/FeedScreen/NavigationOptions.js
+++ app/screens/FeedScreen/NavigationOptions.js
@@ -1,36 +1,5 @@
 import React from 'react';
-import { NavigationActions } from 'react-navigation';
-import { Alert, Button } from 'react-native';
 
-import AuthenticationService from '../../services/AuthenticationService';
-
-export default ({ navigation }) => {
-  function logoutUser() {
-    AuthenticationService.logout().then(() => {
-      navigation.dispatch(NavigationActions.reset({
-        index: 0,
-        actions: [
-          NavigationActions.navigate({ routeName: 'Login' }),
-        ],
-      }));
-    }, console.warn);
-  }
-
-  function onPromptLogoutAlert() {
-    Alert.alert(
-      null,
-      'Are you sure you want to exit?',
-      [
-        { text: 'Yes', onPress: logoutUser },
-        { text: 'Cancel', style: 'cancel' },
-      ],
-    );
-  }
-
-  return {
-    headerRight: (
-      <Button onPress={onPromptLogoutAlert} title="Exit" />
-    ),
-    title: 'DDFA Feed',
-  };
+export default {
+  header: null,
 };
