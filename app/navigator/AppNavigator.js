9a03e14fa4169d8b12fbc48551f7e24927e2eab2
David Vuong <david.vuong256@gmail.com>
2017-06-22 01:20:55 +1000

    rm(SetLocationScreen): replaced with google-places

--- app/navigator/AppNavigator.js
+++ app/navigator/AppNavigator.js
@@ -1,7 +1,6 @@
 import { StackNavigator } from 'react-navigation';
 import LoginScreen from '../screens/LoginScreen/LoginScreen';
 import FeedScreen from '../screens/FeedScreen/FeedScreen';
-import SetLocationScreen from '../screens/SetLocationScreen/SetLocationScreen';
 import CheckInScreen from '../screens/CheckInScreen/CheckInScreen';
 
 export function getNavigator(isLoggedIn) {
@@ -10,7 +9,6 @@ export function getNavigator(isLoggedIn) {
   return StackNavigator({
     Login: { screen: LoginScreen },
     Feed: { screen: FeedScreen },
-    SetLocation: { screen: SetLocationScreen },
     CheckIn: { screen: CheckInScreen },
   }, {
     initialRouteName: isLoggedIn ? 'Feed' : 'Login',
