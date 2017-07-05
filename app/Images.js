f069bf3a29f3e4b4392b2c83d8e5bbcea6ff0c92
David Vuong <david.vuong256@gmail.com>
2017-07-05 18:10:05 +1000

    feat(images): add 5 additional sample restaurant images

--- app/Images.js
+++ app/Images.js
@@ -1,3 +1,9 @@
 export default {
   loginBackgroundImage: require('./images/login_background.jpg'),
+
+  restaurantImage1: require('./images/restaurant_1.jpg'),
+  restaurantImage2: require('./images/restaurant_2.jpg'),
+  restaurantImage3: require('./images/restaurant_3.jpg'),
+  restaurantImage4: require('./images/restaurant_4.jpg'),
+  restaurantImage5: require('./images/restaurant_5.jpg'),
 };
