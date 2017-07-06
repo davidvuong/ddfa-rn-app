a34782c22f1b8e9713e0fef1c8a0b5afe1330a2c
David Vuong <david.vuong256@gmail.com>
2017-07-07 00:32:09 +1000

    refactor(LoginScreen): space out the rgba defs

--- app/screens/LoginScreen/Style.js
+++ app/screens/LoginScreen/Style.js
@@ -13,7 +13,7 @@ export default {
     height: 50,
   },
   inputTextBox: {
-    backgroundColor: 'rgba(255,255,255,0.85)',
+    backgroundColor: 'rgba(255, 255, 255, 0.85)',
   },
   footer: {
     position: 'absolute',
