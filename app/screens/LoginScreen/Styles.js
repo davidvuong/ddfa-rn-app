3d68e156cb1d05826e9bb2ca2a9678df98f3ad9c
David Vuong <david.vuong256@gmail.com>
2017-12-04 01:50:49 +1100

    style(LoginScreen): add very basic styles to login screen

---
+++ app/screens/LoginScreen/Styles.js
@@ -0,0 +1,15 @@
+// @flow
+
+export default {
+  inputLabel: {
+    fontWeight: '500',
+  },
+  inputGroupContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginButtonText: {
+    color: 'white',
+  },
+};
