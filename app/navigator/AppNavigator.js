a1f1057598e0bc300a04429579d5292df010ea29
David Vuong <david.vuong256@gmail.com>
2017-12-03 18:56:19 +1100

    feat(navigator): hooked up FeedScreen to navigator

--- app/navigator/AppNavigator.js
+++ app/navigator/AppNavigator.js
@@ -1,12 +1,14 @@
 // @flow
 import { StackNavigator } from 'react-navigation';
 import LoginScreen from '../screens/LoginScreen/LoginScreen';
+import FeedScreen from '../screens/FeedScreen/FeedScreen';
 
 export default function getNavigator(isLoggedIn: ?boolean) {
   if (isLoggedIn === null) { return null; }
   return StackNavigator({
     Login: { screen: LoginScreen },
+    Feed: { screen: FeedScreen },
   }, {
-    initialRouteName: 'Login',
+    initialRouteName: isLoggedIn ? 'Login' : 'Feed',
   });
 }
