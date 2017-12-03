502d890f0ded7e2f2dc788708925e4f72093c847
David Vuong <david.vuong256@gmail.com>
2017-12-03 17:11:21 +1100

    feat(Screens/LoginScreen): add placeholder login screen

---
+++ app/screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,16 @@
+// @flow
+import { connect } from 'react-redux';
+import LoginComponent from './components/LoginComponent';
+
+const mapStateToProps = (state: *): Object => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch: *): Object => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(LoginComponent);
---
+++ app/screens/LoginScreen/components/LoginComponent.js
@@ -0,0 +1,35 @@
+// @flow
+import * as React from 'react';
+
+import {
+  Container,
+  Text,
+} from 'native-base';
+
+import navigationOptions from '../NavigationOptions';
+
+type Props = {
+
+};
+
+type State = {
+  username: ?string,
+  password: ?string,
+};
+
+export default class LoginComponent extends React.Component<Props, State> {
+  static navigationOptions = navigationOptions;
+
+  constructor(props: Object) {
+    super(props);
+    this.state = { username: null, password: null };
+  }
+
+  render() {
+    return (
+      <Container>
+        <Text>Welcome to the log in screen.</Text>
+      </Container>
+    );
+  }
+}
