b25c1b9555bff0dee03f9deebf1f51c4f6a1eead
David Vuong <david.vuong256@gmail.com>
2017-12-03 18:48:21 +1100

    feat(AuthenticationActions): add auth actions

---
+++ app/actions/AuthenticationActions.js
@@ -0,0 +1,32 @@
+// @flow
+import * as actions from '../ActionTypes';
+import AuthenticationService from '../services/AuthenticationService';
+
+/* Internal */
+
+function loginRequest() {
+  return { type: actions.LOGIN_USER_REQUEST };
+}
+
+function loginSuccess() {
+  return { type: actions.LOGIN_USER_SUCCESS };
+}
+
+function loginError(error: Error) {
+  return { type: actions.LOGIN_USER_ERROR, error };
+}
+
+/* External */
+
+export function loginUser(username: string, password: string) { // eslint-disable-line import/prefer-default-export
+  return (dispatch: *) => {
+    dispatch(loginRequest());
+    return AuthenticationService.login(username, password)
+      .then(() => {
+        dispatch(loginSuccess());
+      }, (error: Error) => {
+        dispatch(loginError(error));
+        throw error;
+      });
+  };
+}
