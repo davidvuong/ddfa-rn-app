698ad42f806fc4cb968ad816bc692950cc01f451
David Vuong <david.vuong256@gmail.com>
2017-12-05 00:36:17 +1100

    feat(authentication): add actions for logoutUser

--- app/ActionTypes.js
+++ app/ActionTypes.js
@@ -4,6 +4,10 @@ export const LOGIN_USER_REQUEST = 'authentication/LOGIN_USER_REQUEST';
 export const LOGIN_USER_SUCCESS = 'authentication/LOGIN_USER_SUCCESS';
 export const LOGIN_USER_ERROR = 'authentication/LOGIN_USER_ERROR';
 
+export const LOGOUT_USER_REQUEST = 'authentication/LOGOUT_USER_REQUEST';
+export const LOGOUT_USER_SUCCESS = 'authentication/LOGOUT_USER_SUCCESS';
+export const LOGOUT_USER_ERROR = 'authentication/LOGOUT_USER_ERROR';
+
 export const CHECK_IN_REQUEST = 'checkIn/CHECK_IN_REQUEST';
 export const CHECK_IN_SUCCESS = 'checkIn/CHECK_IN_SUCCESS';
 export const CHECK_IN_ERROR = 'checkIn/CHECK_IN_ERROR';
