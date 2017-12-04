52ad1f62c9312ac68614eb7f219610d098b36ddb
David Vuong <david.vuong256@gmail.com>
2017-12-05 00:01:02 +1100

    feat(Images): add built-in images and Images mod

---
+++ app/Images.js
@@ -0,0 +1,13 @@
+// @flow
+
+export default {
+  foodImage1: require('./images/food_1.jpg'),
+  foodImage2: require('./images/food_2.jpg'),
+  foodImage3: require('./images/food_3.jpg'),
+  foodImage4: require('./images/food_4.jpg'),
+  foodImage5: require('./images/food_5.jpg'),
+  foodImage6: require('./images/food_6.jpg'),
+  foodImage7: require('./images/food_7.jpg'),
+  foodImage8: require('./images/food_8.jpg'),
+  foodImage9: require('./images/food_9.jpg'),
+};
