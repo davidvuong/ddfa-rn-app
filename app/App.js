221da55114fdc5badd86a6c77bfb5b89269ca127
David Vuong <david.vuong256@gmail.com>
2017-12-04 22:50:53 +1100

    feat(App): use config to build baseApiEndpoint

--- app/App.js
+++ app/App.js
@@ -11,6 +11,7 @@ import {
 import ConfigureStore from './store/ConfigureStore';
 import getReducer from './Reducer';
 import getNavigator from './navigator/AppNavigator';
+import loadConfig from './Config';
 
 import HttpService from './services/HttpService';
 import AuthenticationService from './services/AuthenticationService';
@@ -38,14 +39,17 @@ export default class App extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
+    const config = loadConfig();
+    const baseApiEndpoint = `${config.api.host}:${config.api.port}`;
+
     this.store = ConfigureStore(getReducer());
     this.state = { isLoggedIn: null };
 
     /* Initialize app services. */
     const httpService = new HttpService();
-    AuthenticationService.initialize('localhost', httpService);
+    AuthenticationService.initialize(baseApiEndpoint, httpService);
     AuthenticationService.getTokenFromStorage().then((token: string) => {
-      CheckInService.initialize('localhost', AuthenticationService, httpService);
+      CheckInService.initialize(baseApiEndpoint, AuthenticationService, httpService);
       this.setState({ isLoggedIn: !!token });
     });
   }
