c1303214739b600998a6833b7c1cccfffc2337a5
David Vuong <david.vuong256@gmail.com>
2017-12-05 00:08:51 +1100

    feat(FeedScreen): display first checkinList results
    I'll need to do the infinite scrolling later.

--- app/screens/FeedScreen/components/FeedComponent.js
+++ app/screens/FeedScreen/components/FeedComponent.js
@@ -1,4 +1,5 @@
 // @flow
+import _ from 'lodash';
 import * as React from 'react';
 import {
   Container,
@@ -6,9 +7,17 @@ import {
   Body,
   Content,
   Text,
+  Card,
+  CardItem,
+  Right,
 } from 'native-base';
+import {
+  Image,
+} from 'react-native';
 
 import navigationOptions from '../NavigationOptions';
+import Styles from '../Styles';
+import Images from '../../../Images';
 
 type Props = {
   checkIns: Array<*>,
@@ -25,6 +34,10 @@ type State = {
 export default class FeedScreen extends React.Component<Props, State> {
   static navigationOptions = navigationOptions;
 
+  sampleImages: Array<Buffer>;
+  sampleImagePool: Array<Buffer>;
+  backgroundImageCache: Object;
+
   constructor(props: Props) {
     super(props);
 
@@ -32,7 +45,23 @@ export default class FeedScreen extends React.Component<Props, State> {
       isInitialLoad: false,
       paginationSize: 20,
     };
+
+    this.sampleImages = [
+      Images.foodImage1,
+      Images.foodImage2,
+      Images.foodImage3,
+      Images.foodImage4,
+      Images.foodImage5,
+      Images.foodImage6,
+      Images.foodImage7,
+      Images.foodImage8,
+      Images.foodImage9,
+    ];
+    this.sampleImagePool = [];
+    this.backgroundImageCache = {};
+
     (this: any).performInitialLoad = this.performInitialLoad.bind(this);
+    (this: any).getBackgroundImage = this.getBackgroundImage.bind(this);
   }
 
   componentDidMount() {
@@ -40,6 +69,20 @@ export default class FeedScreen extends React.Component<Props, State> {
     if (!this.props.checkIns.length) {
       this.performInitialLoad();
     }
+  }
+
+  /* Memorization and tries to reduce duplicated image series. */
+  getBackgroundImage(checkInId: string) {
+    const cachedBackgroundImage = this.backgroundImageCache[checkInId];
+    if (cachedBackgroundImage) {
+      return cachedBackgroundImage;
+    }
+    if (!this.sampleImagePool.length) {
+      this.sampleImagePool = _.shuffle(_.cloneDeep(this.sampleImages));
+    }
+    const backgroundImage = this.sampleImagePool.shift();
+    this.backgroundImageCache[checkInId] = backgroundImage;
+    return backgroundImage;
   }
 
   performInitialLoad() {
@@ -51,7 +94,6 @@ export default class FeedScreen extends React.Component<Props, State> {
   }
 
   render() {
-    console.log(this.props);
     return (
       <Container>
         <Header>
@@ -59,9 +101,35 @@ export default class FeedScreen extends React.Component<Props, State> {
             <Text>DDFA Feed</Text>
           </Body>
         </Header>
-        <Content padder>
-          <Text>Welcome to the feed page!</Text>
-        </Content>
+          <Content padder removeClippedSubviews={true}>
+            {
+              _.map(this.props.checkIns, (checkIn: *) => {
+                return (
+                  <Card key={checkIn.id}>
+                    <CardItem>
+                      <Body>
+                        <Text numberOfLines={1}>{checkIn.name}</Text>
+                        <Text note numberOfLines={1}>{checkIn.address}</Text>
+                      </Body>
+                    </CardItem>
+                    <CardItem cardBody>
+                      <Image source={this.getBackgroundImage(checkIn.id)} style={{ height: 180, width: null, flex: 1 }} />
+                      <Text note numberOfLines={1} style={{
+                        position: 'absolute',
+                        bottom: 3,
+                        right: 3,
+                        backgroundColor: 'rgba(0, 0, 0, 0)',
+                        color: 'white',
+                        fontWeight: '500',
+                        fontStyle: 'italic',
+                        fontSize: 10,
+                      }}>{checkIn.createdAt}</Text>
+                    </CardItem>
+                  </Card>
+                );
+              })
+            }
+          </Content>
       </Container>
     );
   }
