6e831c6867565199a43a8ef55845a8196fe131a9
David Vuong <david.vuong256@gmail.com>
2017-12-08 00:43:46 +1100

    feat(AppNavigator): add CheckInDetailScreen to navigator

--- app/navigator/AppNavigator.js
+++ app/navigator/AppNavigator.js
@@ -2,12 +2,15 @@
 import { StackNavigator } from 'react-navigation';
 import LoginScreen from '../screens/LoginScreen/LoginScreen';
 import CheckInListScreen from '../screens/CheckInListScreen/CheckInListScreen';
+import CheckInDetailScreen from '../screens/CheckInDetailScreen/CheckInDetailScreen';
 
 export default function getNavigator(isLoggedIn: ?boolean) {
   if (isLoggedIn === null) { return null; }
   return StackNavigator({
     Login: { screen: LoginScreen },
     CheckInList: { screen: CheckInListScreen },
+    CheckInDetail: { screen: CheckInDetailScreen },
+    // CheckInCreate: { screen: CheckInCreateScreen },
   }, {
     initialRouteName: isLoggedIn ? 'CheckInList' : 'Login',
   });
