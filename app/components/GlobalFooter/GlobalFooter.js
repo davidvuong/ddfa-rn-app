f2f08dde2aed93a848eec71cb12e475305a10162
David Vuong <david.vuong256@gmail.com>
2018-01-01 13:41:07 +1100

    feat+refactor(GlobalFooter): add checkInCreate and split cmp

--- app/components/GlobalFooter/GlobalFooter.js
+++ app/components/GlobalFooter/GlobalFooter.js
@@ -1,111 +1,26 @@
 // @flow
 import { connect } from 'react-redux';
-import * as React from 'react';
-import { NavigationActions } from 'react-navigation';
-import {
-  Text,
-  Icon,
-  Footer,
-  FooterTab,
-  Button,
-} from 'native-base';
-import {
-  Alert,
-} from 'react-native';
-import RNGooglePlaces from 'react-native-google-places';
-
 import {
   setSelectedLocation,
+  createCheckIn,
 } from '../../actions/CheckInActions';
 import {
   logoutUser,
 } from '../../actions/AuthenticationActions';
-
-type Props = {
-  navigation: *,
-  logoutUser: () => *,
-};
-
-type State = {
-};
-
-export class GlobalFooterComponent extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    (this: any).navigateToLogin = this.navigateToLogin.bind(this);
-    (this: any).onPressLogout = this.onPressLogout.bind(this);
-    (this: any).onPressCheckIn = this.onPressCheckIn.bind(this);
-  }
-
-  navigateToLogin() {
-    this.props.logoutUser()
-      .then(() => {
-        this.props.navigation.dispatch(NavigationActions.reset({
-          index: 0,
-          actions: [
-            NavigationActions.navigate({ routeName: 'Login' }),
-          ],
-        }));
-      })
-      .catch((error: Error) => { console.error(error.message); });
-  }
-
-  onPressLogout() {
-    const buttons = [
-      { text: 'Yes', onPress: this.navigateToLogin },
-      { text: 'Cancel', style: 'cancel' },
-    ];
-    Alert.alert('Exit DDFA', 'Are you sure you want to log out?', buttons);
-  }
-
-  onPressCheckIn() {
-    const options = { radius: 0.3 };
-    RNGooglePlaces.openPlacePickerModal(options)
-      .then((place: *) => {
-        this.props.setSelectedLocation({
-          address: place.address || place.name,
-          latitude: place.latitude,
-          longitude: place.longitude,
-          place,
-        });
-        this.props.navigation.navigate('CheckInCreate');
-      })
-      .catch((error: Error) => { console.log(error.message); });
-  }
-
-  render() {
-    return (
-      <Footer>
-        <FooterTab>
-          <Button vertical>
-            <Icon name="apps" />
-            <Text>home</Text>
-          </Button>
-          <Button vertical>
-            <Icon name="search" />
-            <Text>search</Text>
-          </Button>
-          <Button vertical onPress={this.onPressCheckIn}>
-            <Icon name="pin" />
-            <Text>check-in</Text>
-          </Button>
-          <Button vertical onPress={this.onPressLogout}>
-            <Icon name="exit" />
-            <Text>logout</Text>
-          </Button>
-        </FooterTab>
-      </Footer>
-    );
-  }
-}
+import GlobalFooterComponent from './GlobalFooterComponent';
 
 const mapStateToProps = (state: *) => {
-  return {};
+  const { checkIn } = state;
+  return {
+    isCheckingIn: checkIn.isCheckingIn,
+  };
 };
 
 const mapDispatchToProps = (dispatch: *) => {
   return {
+    createCheckIn: (latitude: number, longitude: number, address: string, name: string) => {
+      return dispatch(createCheckIn(latitude, longitude, address, name));
+    },
     logoutUser: () => {
       return dispatch(logoutUser());
     },
---
+++ app/components/GlobalFooter/GlobalFooterComponent.js
@@ -0,0 +1,97 @@
+// @flow
+import * as React from 'react';
+import { NavigationActions } from 'react-navigation';
+import {
+  Text,
+  Icon,
+  Footer,
+  FooterTab,
+  Button,
+} from 'native-base';
+import {
+  Alert,
+} from 'react-native';
+import RNGooglePlaces from 'react-native-google-places';
+
+type Props = {
+  navigation: *,
+  logoutUser: () => *,
+  createCheckIn: (number, number, string, string) => *,
+};
+
+type State = {};
+
+export default class GlobalFooterComponent extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    (this: any).navigateToLogin = this.navigateToLogin.bind(this);
+    (this: any).onPressLogout = this.onPressLogout.bind(this);
+    (this: any).onPressCheckIn = this.onPressCheckIn.bind(this);
+  }
+
+  navigateToLogin() {
+    this.props.logoutUser()
+      .then(() => {
+        this.props.navigation.dispatch(NavigationActions.reset({
+          index: 0,
+          actions: [
+            NavigationActions.navigate({ routeName: 'Login' }),
+          ],
+        }));
+      })
+      .catch((error: Error) => { console.error(error.message); });
+  }
+
+  onPressLogout() {
+    const buttons = [
+      { text: 'Yes', onPress: this.navigateToLogin },
+      { text: 'Cancel', style: 'cancel' },
+    ];
+    Alert.alert('Exit DDFA', 'Are you sure you want to log out?', buttons);
+  }
+
+  onPressCheckIn() {
+    const options = { radius: 0.3 };
+    RNGooglePlaces.openPlacePickerModal(options)
+      .then((place: *) => {
+        // NOTE:
+        //
+        // Replace setSelectedLocation to checkIn (but also include the GooglePlace)
+        // Perhaps... `setGooglePlace`, `setCheckIn`.
+        this.props.setSelectedLocation({
+          address: place.address || place.name,
+          latitude: place.latitude,
+          longitude: place.longitude,
+          place,
+        });
+        this.props.navigation.navigate('CheckInCreate');
+      })
+      .catch((error: Error) => { console.error(error.message); });
+  }
+
+  render() {
+    return (
+      <Footer>
+        <FooterTab>
+          <Button vertical>
+            <Icon name="apps" />
+            <Text>home</Text>
+          </Button>
+          <Button vertical>
+            <Icon name="search" />
+            <Text>search</Text>
+          </Button>
+          <Button vertical onPress={this.onPressCheckIn}>
+            <Icon name="pin" />
+            <Text>check-in</Text>
+          </Button>
+          <Button vertical onPress={this.onPressLogout}>
+            <Icon name="exit" />
+            <Text>logout</Text>
+          </Button>
+        </FooterTab>
+      </Footer>
+    );
+  }
+}
