9c98f83c0ca71dc58fe838985d538a0a0e25973c
David Vuong <david.vuong256@gmail.com>
2018-01-02 23:44:32 +1100

    syntax(AppNavigator): let flow decide on the StackNavigator type

--- app/navigator/AppNavigator.js
+++ app/navigator/AppNavigator.js
@@ -6,7 +6,7 @@ import CheckInListScreen from '../screens/CheckInListScreen/CheckInListScreen';
 import CheckInDetailScreen from '../screens/CheckInDetailScreen/CheckInDetailScreen';
 import CheckInCreateScreen from '../screens/CheckInCreateScreen/CheckInCreateScreen';
 
-export default function getNavigator(isLoggedIn: ?boolean): ?StackNavigator {
+export default function getNavigator(isLoggedIn: ?boolean): * {
   if (isLoggedIn === null) { return null; }
   return StackNavigator({
     Login: { screen: LoginScreen },
