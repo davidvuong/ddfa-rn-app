dcd7a7c6d3426f770484edbc994936fc45a00a31
David Vuong <david.vuong256@gmail.com>
2018-01-14 13:27:54 +1100

    feat(CheckInNearbyComponent): add empty CheckInNearbyCmp

--- app/screens/CheckInNearbyScreen/components/CheckInNearbyComponent.js
+++ app/screens/CheckInNearbyScreen/components/CheckInNearbyComponent.js
@@ -1,27 +1,60 @@
 // @flow
 import * as React from 'react';
 import {
+  Body,
+  Header,
   Container,
   Content,
+  Text,
 } from 'native-base';
+import {
+  ActivityIndicator,
+} from 'react-native';
 
 import GlobalFooter from '../../../components/GlobalFooter/GlobalFooter';
+import CheckInNearbyMap from './CheckInNearbyMap/CheckInNearbyMap';
 import navigationOptions from '../NavigationOptions';
-import Styles from '../Styles';
 
 type Props = {
   navigation: *,
 };
 
-type State = {};
+type State = {
+  currentLocation: ?*,
+};
 
 export default class CheckInNearbyComponent extends React.Component<Props, State> {
   static navigationOptions = navigationOptions;
+  state = {
+    currentLocation: null,
+  };
+
+  renderContent() {
+    if (!this.state.currentLocation) {
+      return (
+        <Content>
+          <ActivityIndicator color="black" />
+        </Content>
+      );
+    }
+
+    const { latitude, longitude } = this.state.currentLocation.coords;
+    return (
+      <Content>
+        <CheckInNearbyMap latitude={latitude} longitude={longitude} />
+      </Content>
+    );
+  }
 
   render() {
     return (
       <Container>
-        <Content />
+        <Header>
+          <Body>
+            <Text>DDFA Check-ins Nearby</Text>
+          </Body>
+        </Header>
+        {this.renderContent()}
         <GlobalFooter navigation={this.props.navigation} />
       </Container>
     );
