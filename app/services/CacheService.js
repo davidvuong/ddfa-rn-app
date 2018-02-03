84fbdc13e06c26418d1d1715ceb09c26ea87d921
David Vuong <david.vuong256@gmail.com>
2018-02-03 19:01:38 +1100

    feat(CacheService): now deals in Objects rather than strings

--- app/services/CacheService.js
+++ app/services/CacheService.js
@@ -6,18 +6,18 @@ import {
 
 /* A simple wrapper over React Native's AsyncStorage (with bluebird.Promises). */
 export default class CacheService {
-  set = (key: string, value: string): Promise<*> => {
+  set = (key: string, value: Object): Promise<*> => {
     return new Promise((resolve: *, reject: *) => {
-      AsyncStorage.setItem(key, value, (error: Error) => {
+      AsyncStorage.setItem(key, JSON.stringify(value), (error: Error) => {
         return error ? reject(error) : resolve();
       });
     });
   }
 
-  get = (key: string): Promise<?string> => {
+  get = (key: string): Promise<?Object> => {
     return new Promise((resolve: *, reject: *) => {
       AsyncStorage.getItem(key, (error: Error, item: ?string) => {
-        return error ? reject(error) : resolve(item || null);
+        return error ? reject(error) : resolve(item ? JSON.parse(item) : null);
       });
     });
   }
