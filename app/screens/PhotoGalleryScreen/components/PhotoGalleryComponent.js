41fab0c8b47f22547e7c371d35d5d15caf0eb31f
David Vuong <david.vuong256@gmail.com>
2018-05-06 21:52:04 +1000

    rm(PHotoGallery): get rid of ImageCarousel

--- app/screens/PhotoGalleryScreen/components/PhotoGalleryComponent.js
+++ app/screens/PhotoGalleryScreen/components/PhotoGalleryComponent.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import _ from 'lodash';
 import {
   Container,
   Header,
@@ -8,16 +7,12 @@ import {
   Text,
 } from 'native-base';
 
-import { getRandomFood } from '../../../Images';
 import GlobalFooter from '../../../components/GlobalFooter/GlobalFooter';
-import ImageCarousel from '../../../components/ImageCarousel/ImageCarousel';
 import navigationOptions from '../NavigationOptions';
 import Styles from '../Styles';
 
 export default class LoginComponent extends React.Component {
   static navigationOptions = navigationOptions;
-
-  IMAGES = _.range(10, () => { return getRandomFood(); });
 
   render() {
     return (
@@ -29,7 +24,6 @@ export default class LoginComponent extends React.Component {
         </Header>
         <Content padder scrollEnabled={false} bounces={false}>
           <Text>Comming soon...</Text>
-          <ImageCarousel images={this.IMAGES} />
         </Content>
         <GlobalFooter navigation={this.props.navigation} />
       </Container>
